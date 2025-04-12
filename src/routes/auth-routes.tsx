182e2af3fe980c3c45e15573ebe69982560e3963
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-12 12:08:50 -0300

    feat: creating the authlayout

--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router";
+
+import { AuthLayout } from "../components/AuthLayout";
 import { SignIn } from "../pages/SignIn";
 
 export const AuthRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<SignIn />} />
+            <Route path="/" element={<AuthLayout />}>
+                <Route path="/" element={<SignIn />} />
+            </Route>
         </Routes>
     )
 }
