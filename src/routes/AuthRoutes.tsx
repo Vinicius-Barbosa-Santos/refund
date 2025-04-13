4fbbd9f22368ee9516bf2c56efca97784b7737d6
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-13 12:57:48 -0300

    feat: creating signup

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 
 import { AuthLayout } from "../components/AuthLayout";
 import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
 
 export const AuthRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<AuthLayout />}>
                 <Route path="/" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
             </Route>
         </Routes>
     )
