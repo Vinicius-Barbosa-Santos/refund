b65b0ebcc995185f7a0218b8aba76d442445a6b5
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-13 13:08:00 -0300

    feat: creating not found

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import { AuthLayout } from "../components/AuthLayout";
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
+import { NotFound } from "../pages/NotFound";
 
 export const AuthRoutes = () => {
     return (
@@ -11,6 +12,8 @@ export const AuthRoutes = () => {
                 <Route path="/" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
             </Route>
+
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
