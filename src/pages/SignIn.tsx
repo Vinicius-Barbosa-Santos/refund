02f52839d5ff14f4a5aaecefcdcd6be710a75404
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-13 12:42:02 -0300

    feat: signup link

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export const SignIn = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log(email, password)
+    }
+
     return (
-        <form className="w-full flex flex-col gap-4">
+        <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
             <Input
                 required
                 legend="E-mail"
                 type="email"
                 placeholder="seu@gmail.com"
+                onChange={(e) => setEmail(e.target.value)}
             />
 
             <Input
@@ -16,11 +27,20 @@ export const SignIn = () => {
                 legend="Senha"
                 type="password"
                 placeholder="123456"
+                onChange={(e) => setPassword(e.target.value)}
             />
 
-            <Button isLoading>
+            <Button isLoading={isLoading} type="submit">
                 Entrar
             </Button>
+
+            <a href="/signup"
+                className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center 
+                hover:text-green-800 transition 
+                ease-linear"
+            >
+                Criar Conta
+            </a>
         </form>
     );
 }
