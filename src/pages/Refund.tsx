b3d36b98a5fe20cf446ab365d015e452398b8303
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-17 22:06:08 -0300

    feat: populating select with categories

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
 
 export const Refund = () => {
+
+    const [category, setCategory] = useState("")
+
     return (
         <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
             <header>
@@ -16,8 +21,16 @@ export const Refund = () => {
 
             <Select
                 required
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
                 legend="Categoria"
-            />
+            >
+                {
+                    CATEGORIES_KEYS.map((category) => (
+                        <option value={category} key={category}>{CATEGORIES[category].name}</option>
+                    ))
+                }
+            </Select>
         </form>
     )
 }
