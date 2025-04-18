e9836e2bec366b8200c6abf9031481a1d797e112
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-18 16:29:56 -0300

    feat: finalizing confirm

---
+++ src/pages/Confirm.tsx
@@ -0,0 +1,26 @@
+import { Navigate, useLocation } from "react-router";
+import okSvg from "../assets/ok.svg";
+
+const Confirm = () => {
+    const location = useLocation()
+
+    if (!location.state?.fromSubmit) {
+        return <Navigate to="/" />
+    }
+
+    return (
+        <div className="bg-gray-500 lg:w-[512px] rounded-xl flex flex-col items-center p-10 gap-6">
+            <h1 className="text-2xl font-bold text-center text-green-100">Solicitação Enviada</h1>
+            <img src={okSvg} alt="Ícone de OK" className="w-28" />
+            <p className="text-sm text-gray-200 text-center">
+                Agora é apenas aguardar! Sua SOlicitação será analisada e, em breve, o setor financeiro irá entrar em contato com você.
+            </p>
+
+            <a className="w-full p-3 text-center bg-green-100 rounded-lg text-white hover:bg-green-200 transition ease-linear" href="/">
+                Nova solicitação
+            </a>
+        </div>
+    );
+}
+
+export default Confirm;
--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -2,13 +2,32 @@ import { useState } from "react"
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
+import { Upload } from "../components/Upload"
+import { Button } from "../components/Button"
+import { useNavigate } from "react-router"
 
 export const Refund = () => {
-
+    const [name, setName] = useState("")
+    const [amount, setAmount] = useState("")
     const [category, setCategory] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [filename, setFilename] = useState<File | null>(null)
 
+    const navigate = useNavigate()
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log({
+            name,
+            amount,
+            category,
+            filename
+        })
+
+        navigate("/confirm", { state: { fromSubmit: true } })
+    }
     return (
-        <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
+        <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
             <header>
                 <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
                 <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar o reembolso. </p>
@@ -17,20 +36,40 @@ export const Refund = () => {
             <Input
                 required
                 legend="Nome da solicitação"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
             />
 
-            <Select
-                required
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-                legend="Categoria"
-            >
-                {
-                    CATEGORIES_KEYS.map((category) => (
-                        <option value={category} key={category}>{CATEGORIES[category].name}</option>
-                    ))
-                }
-            </Select>
+            <div className="flex gap-4">
+                <Select
+                    required
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    legend="Categoria"
+                >
+                    {
+                        CATEGORIES_KEYS.map((category) => (
+                            <option value={category} key={category}>{CATEGORIES[category].name}</option>
+                        ))
+                    }
+                </Select>
+
+                <Input
+                    required
+                    legend="Valor"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                />
+            </div>
+
+            <Upload
+                filename={filename && filename.name}
+                onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+            />
+
+            <Button isLoading={isLoading} type="submit">
+                Enviar
+            </Button>
         </form>
     )
 }
