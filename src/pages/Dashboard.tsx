92cd1777b8ab5f1d2f1232d6987ff3ab469f5c3e
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-20 03:50:14 -0300

    feat: search input

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import { Input } from "../components/Input";
+
 export const Dashboard = () => {
+    const [name, setName] = useState("")
+
+    const fetchRefunds = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log(name)
+    }
+
     return (
         <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
             <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
+
+            <form onSubmit={fetchRefunds} className="flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6">
+                <Input onChange={(e) => setName(e.target.value)} placeholder="Pesquisar pelo nome" />
+            </form>
         </div>
     );
 }
