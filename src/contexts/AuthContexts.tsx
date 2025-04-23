4ddac3bf555806684ed038063b8f8a684763c276
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-23 15:45:49 -0300

    feat: local storage

--- src/contexts/AuthContexts.tsx
+++ src/contexts/AuthContexts.tsx
@@ -5,12 +5,16 @@ type AuthContextProps = {
     save: (data: UserAPIResponse) => void
 }
 
+const LOCAL_STORAGE_KEY = "@refund"
+
 export const AuthContexts = createContext({} as AuthContextProps)
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [session, setSession] = useState<null | UserAPIResponse>(null)
 
     const save = (data: UserAPIResponse) => {
+        localStorage.setItem(`${LOCAL_STORAGE_KEY}:user`, JSON.stringify(data.user))
+        localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token)
         setSession(data)
     }
 
