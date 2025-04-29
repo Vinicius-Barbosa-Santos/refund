3b57a78eef3368c6314503221382dd9c02cc5e6f
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-04-29 17:05:04 -0300

    feat: loading request data

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
@@ -11,6 +11,7 @@ import fileSvg from '../assets/file.svg'
 import * as z from "zod"
 import { AxiosError } from "axios"
 import { api } from "../services/api"
+import { formatCurrency } from "../utils/formatCurrency"
 
 const refundSchema = z.object({
     name: z.string().min(3, { message: "Informe um nome claro para a sua solicitação" }),
@@ -24,6 +25,7 @@ export const Refund = () => {
     const [category, setCategory] = useState("")
     const [isLoading, setIsLoading] = useState(false)
     const [file, setFile] = useState<File | null>(null)
+    const [fileURL, setFileURL] = useState<string | null>(null)
 
     const navigate = useNavigate()
     const params = useParams<{ id: string }>()
@@ -72,6 +74,31 @@ export const Refund = () => {
             setIsLoading(false)
         }
     }
+
+    const fetchRefund = async (id: string) => {
+        try {
+            const { data } = await api.get<RefundAPIResponse>(`/refunds/${id}`)
+            setName(data.name)
+            setCategory(data.category)
+            setAmount(formatCurrency(data.amount))
+            setFileURL(data.filename)
+        } catch (error) {
+            console.log(error)
+
+            if (error instanceof AxiosError) {
+                return alert(error.response?.data.message)
+            }
+
+            alert("Não foi possível carregar")
+        }
+    }
+
+    useEffect(() => {
+        if (params.id) {
+            fetchRefund(params.id)
+        }
+    }, [params.id])
+
     return (
         <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
             <header>
@@ -111,7 +138,7 @@ export const Refund = () => {
                 />
             </div>
 
-            {params.id ? (
+            {params.id && fileURL ? (
                 <a href="https://www.rocketseat.com.br/" target="_blank" className="text-sm text-green-100 
                     font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear">
                     <img src={fileSvg} alt="Ícone do arquivo" />
